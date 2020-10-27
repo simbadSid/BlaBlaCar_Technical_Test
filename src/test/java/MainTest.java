6ee872a0a7f4c997c7647ee88d7bb8d243bb219f
riyane <riyane@localhost.localdomain>
2020-10-27 04:33:58 +0100

    Add a long test scenario

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -33,6 +33,12 @@ public class MainTest extends TestCase
         assertTrue(compareInput_expectedOutput("Collision_OutField_Mower5_X5_Y5_0"));
     }
 
+    public void testMain_Collision_OutField_Mower5_X5_Y5_1() throws InterruptedException, IOException
+    {
+        // Lunched without comparing it to a known solution
+        runSimulation("Collision_OutField_Mower5_X5_Y5_1");
+    }
+
     public void testMain_NoCollision_NoOutField_Mower3_X5_Y5_0() throws InterruptedException, IOException
     {
         assertTrue(compareInput_expectedOutput("NoCollision_NoOutField_Mower3_X5_Y5_0"));
@@ -52,7 +58,7 @@ public class MainTest extends TestCase
     // ====================================
     // Private methods
     // ====================================
-    private boolean compareInput_expectedOutput(String inputFile) throws InterruptedException, IOException
+    private String[] runSimulation(String inputFile) throws InterruptedException
     {
         String[] args = new String[2];
         File dir = new File(PATH_INPUT_FILE);
@@ -62,7 +68,12 @@ public class MainTest extends TestCase
         args[1] = PATH_INPUT_FILE + inputFile;
 
         // Run the simulation
-        String[] simulationResult = Main.runSimulation(args);
+        return Main.runSimulation(args);
+    }
+    private boolean compareInput_expectedOutput(String inputFile) throws InterruptedException, IOException
+    {
+        // Run the simulation
+        String[] simulationResult = runSimulation(inputFile);
 
         //Compare the result with the expected result
         File            outputFile  = new File(PATH_OUTPUT_FILE + inputFile);
